34af58354949d94ce8199fce8394cda2db2c262d
Adel Hassan <sean.dashals@gmail.com>
2023-08-22 20:20:51 -0500

    fix record name

--- team_code.py
+++ team_code.py
@@ -351,7 +351,7 @@ def get_features(data_folder, patient_id, vae):
         rec_latents = []
         rec_hrs = []
         for record_num in records:
-            rec = wfdb.io.rdrecord(os.path.join(pt_dir, record_num),
+            rec = wfdb.io.rdrecord(os.path.join(pt_dir, record_num + '_EEG'),
                                    channel_names=wandb.config.channels)
             sig = rec.p_signal
             scaled_sig = scaler.fit_transform(sig)
